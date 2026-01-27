fd5cee96a13cb1c2400730bc2b5eab61b01c0931
peet5228 <ppeet5228@gmail.com>
2026-01-27 13:11:30 +0700

    update

---
+++ backend/routes/Staff/member.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bc = require('bcryptjs')
+const router = express.Router()
+const db = require('../../db')
+const {verifyToken,requireRole} = require('../../middleware/authMiddleware')
+
+// API สำหรับ Get ข้อมูล
+router.get('/eva',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const [rows] = await db.query(`select id_member,first_name,last_name,email,username,role where role='ผู้รับการประเมินผล' order by id_member desc`)
+        res.json(rows)
+    }catch(err){
+        console.error('Error Get',err)
+        res.status(500).json({message:'Error Get'})
+    }
+})
+
+// API สำหรับ Get ข้อมูล
+router.get('/commit',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const [rows] = await db.query(`select id_member,first_name,last_name,email,username,role where role='กรรมการประเมิน' order by id_member desc`)
+        res.json(rows)
+    }catch(err){
+        console.error('Error Get',err)
+        res.status(500).json({message:'Error Get'})
+    }
+})
+
+// API สำหรับ Get ข้อมูล where params
+router.get('/:id_member',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
+    try{
+        const {id_member} = req.params
+        const [rows] = await db.query(`select id_member,first_name,last_name,email,username,role where id_member='${id_member}' order by id_member desc`)
+        if(rows.length === 0) return res.status(403).json({message:'ไม่พบข้อมูล'})
+        res.json(rows)
+    }catch(err){
+        console.error('Error Get',err)
+        res.status(500).json({message:'Error Get'})
+    }
+})
+
+module.exports = router
